3110a39852c616b648f48be9d91fe480b491f9ee
Roberto Heckers <robertoheckers@gmail.com>
2023-08-15 15:53:59 +0200

    Initial routing setup

--- business-tools-monorepo/apps/finance/expenses-registration/src/app/app.routes.ts
+++ business-tools-monorepo/apps/finance/expenses-registration/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'expenses-overview', pathMatch: 'full' },
+  {
+    path: 'expenses-overview',
+    loadComponent: () => import('../pages/expenses-overview-page/expenses-overview-page.component')
+  }
+];
