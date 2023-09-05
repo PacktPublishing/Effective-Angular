197675e048aea765623f8c5abba72ddb5a01de41
R Heckers <35544104+RHeckers@users.noreply.github.com>
2023-09-05 20:11:01 +0200

    Feature/chapter two/dependency injection (#5)
    * Create new library
    
    * Create expenses service

---
+++ business-tools-monorepo/libs/finance/data-access/expenses/src/lib/servies/expenses.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ExpensesService } from './expenses.service';
+
+describe('ExpensesService', () => {
+  let service: ExpensesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ExpensesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
