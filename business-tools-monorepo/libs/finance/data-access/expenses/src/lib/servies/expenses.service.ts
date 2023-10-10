1396b9a8485a7256747b0a63353b25e68fbc1294
Roberto Heckers <robertoheckers@gmail.com>
2023-10-10 12:00:21 +0200

    Remove unused code

--- business-tools-monorepo/libs/finance/data-access/expenses/src/lib/servies/expenses.service.ts
+++ business-tools-monorepo/libs/finance/data-access/expenses/src/lib/servies/expenses.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { interval } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ExpensesService {
-}
+export class ExpensesService { }
