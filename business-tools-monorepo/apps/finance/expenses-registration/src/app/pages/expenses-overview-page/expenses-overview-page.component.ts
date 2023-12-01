e449363e1c7fcca774a89d2cc7e29729d542d707
Roberto Heckers <robertoheckers@gmail.com>
2023-12-01 12:02:30 +0100

    Update to Angular 17

--- business-tools-monorepo/apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
+++ business-tools-monorepo/apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddExpenseComponent, AddExpenseReactive } from '@bt-libs/finance/ui/expenses-registration-forms';
-import { DisplayScalesComponent, ModalComponent, ScalesProjectionDirective, SelectComponent, WidgetContainerComponent } from '@bt-libs/shared/common-components';
+import { ModalComponent } from '@bt-libs/shared/common-components';
 
 @Component({
   selector: 'business-tools-monorepo-expenses-overview-page',
   standalone: true,
-  imports: [CommonModule, AddExpenseComponent, ModalComponent, DisplayScalesComponent, ScalesProjectionDirective, SelectComponent, WidgetContainerComponent],
+  imports: [CommonModule, AddExpenseComponent, ModalComponent],
   templateUrl: './expenses-overview-page.component.html',
   styleUrls: ['./expenses-overview-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,9 +18,5 @@ export default class ExpensesOverviewPageComponent {
 
   onAddExpense(expense: AddExpenseReactive) {
     console.log('addExpense ==>', expense);
-  }
-
-  onOptionChange(value: any) {
-    console.log('onOptionChange ==>', value);
   }
 }
