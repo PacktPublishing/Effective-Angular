6e75d6fb4543d29a53bc57d17ad2f3864e8c6bfd
Roberto Heckers <robertoheckers@gmail.com>
2024-01-03 16:48:14 +0100

    rename store file

--- business-tools-monorepo/libs/finance/data-access/expenses/src/lib/stores/expenses.store.spec.ts
+++ business-tools-monorepo/libs/finance/data-access/expenses/src/lib/stores/expenses.store.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ExpensesStoreService } from './expenses.store.service';
+import { ExpensesStoreService } from './expenses.store';
 
 describe('ExpensesStoreService', () => {
   let service: ExpensesStoreService;
