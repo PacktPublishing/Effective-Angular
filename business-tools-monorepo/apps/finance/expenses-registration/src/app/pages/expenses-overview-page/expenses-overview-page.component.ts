ae4f1a88b318cb6a1919840576504786a037039b
Roberto Heckers <robertoheckers@gmail.com>
2024-01-03 16:46:23 +0100

    Add store and cleanup component

--- business-tools-monorepo/apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
+++ business-tools-monorepo/apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
@@ -1,10 +1,7 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddExpenseComponent, AddExpenseReactive } from '@bt-libs/finance/ui/expenses-registration-forms';
 import { ModalComponent } from '@bt-libs/shared/common-components';
-import { LogMethod } from '@bt-libs/shared/util/custom-decorators';
-import { ExpensesHttpService } from '@bt-libs/finance/data-access/expenses';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 
 @Component({
@@ -19,21 +16,16 @@ export default class ExpensesOverviewPageComponent {
   addExpenseShown = false;
 
   protected readonly cd = inject(ChangeDetectorRef);
-  protected readonly expensesApi = inject(ExpensesHttpService);
-  protected readonly destroyRef = inject(DestroyRef);
+  // protected readonly expensesApi = inject(ExpensesHttpService);
+  // protected readonly destroyRef = inject(DestroyRef);
 
   onAddExpense(expense: AddExpenseReactive) {
     console.log('addExpense ==>', expense);
   }
 
-  ngOnInit() {
-    this.test(1, 2);
-    // eslint-disable-next-line rxjs/no-ignored-error, rxjs-angular/prefer-takeuntil
-    this.expensesApi.get().pipe(takeUntilDestroyed(this.destroyRef)).subscribe((data) => { console.log('data ==>', data); });
-  }
+  // ngOnInit() {
+  //   // eslint-disable-next-line rxjs/no-ignored-error, rxjs-angular/prefer-takeuntil
+  //   this.expensesApi.get().pipe(takeUntilDestroyed(this.destroyRef)).subscribe((data) => { console.log('data ==>', data); });
+  // }
 
-  @LogMethod
-  test(a: number, b: number) {
-    return a + b;
-  }
 }
