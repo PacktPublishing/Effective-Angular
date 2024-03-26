0a2a0fa8ee85c744a1cbfeef4efb7bcf97e2e34d
R Heckers <35544104+RHeckers@users.noreply.github.com>
2024-03-26 22:02:00 +0100

    Feature/chapter ten/internationalization accessibility (#24)
    * adjust generator
    
    * Transloco setup
    
    * Internationalization and localization

--- business-tools-monorepo/apps/finance/expenses-registration/src/app/app.config.ts
+++ business-tools-monorepo/apps/finance/expenses-registration/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {
   TitleStrategy,
   provideRouter,
@@ -11,6 +11,9 @@ import { MockInterceptor } from '@bt-libs/shared/data-access/generic-http';
 import { provideState, provideStore } from '@ngrx/store';
 import { expensesFeatureKey, expensesReducer, ExpensesEffects } from '@bt-libs/finance/data-access/expenses';
 import { provideEffects } from '@ngrx/effects';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@ngneat/transloco';
+import { provideTranslocoLocale } from '@ngneat/transloco-locale';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,6 +27,23 @@ export const appConfig: ApplicationConfig = {
       provide: TitleStrategy,
       useClass: TemplatePageTitleStrategy,
     },
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'nl'],
+        defaultLang: 'en',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader
+    }),
+    provideTranslocoLocale({
+      langToLocaleMapping: {
+        en: 'en-US',
+        nl: 'nl-NL'
+      },
+      // localeToCurrencyMapping: {}
+    })
   ],
 };
 // MockInterceptor
--- business-tools-monorepo/apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
+++ business-tools-monorepo/apps/finance/expenses-registration/src/app/pages/expenses-overview-page/expenses-overview-page.component.ts
@@ -1,19 +1,31 @@
 import { ExpenseModel, ExpensesFacade } from '@bt-libs/finance/data-access/expenses';
-import { ChangeDetectionStrategy, Component, OnInit, computed, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, computed, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddExpenseComponent } from '@bt-libs/finance/ui/expenses-registration-forms';
 import { ModalComponent } from '@bt-libs/shared/common-components';
+import { TranslocoDirective } from '@ngneat/transloco';
+import { TranslationService } from '../../translation.serive';
+import { TranslocoCurrencyPipe, TranslocoDatePipe, TranslocoDecimalPipe } from '@ngneat/transloco-locale';
 
 @Component({
   selector: 'business-tools-monorepo-expenses-overview-page',
   standalone: true,
-  imports: [CommonModule, AddExpenseComponent, ModalComponent],
+  imports: [CommonModule, AddExpenseComponent, ModalComponent, TranslocoDirective, TranslocoCurrencyPipe, TranslocoDatePipe, TranslocoDecimalPipe],
   templateUrl: './expenses-overview-page.component.html',
   styleUrls: ['./expenses-overview-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class ExpensesOverviewPageComponent implements OnInit {
   protected readonly expensesFacade = inject(ExpensesFacade);
+  protected readonly translationService = inject(TranslationService);
+
+  x = 123456789.10
+
+  translationEventsEffect = effect(() => {
+    if (this.translationService.translationsLoaded()) {
+      console.log('==>', this.translationService.translocoService.translate('expenses_overview_page.title'));
+    }
+  });
 
   expenses = this.expensesFacade.expenses;
 
