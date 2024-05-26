953a8daa8bac94c235e34f5cc6e0655da5a630aa
R Heckers <35544104+RHeckers@users.noreply.github.com>
2024-05-26 15:44:44 +0200

    Feature/chapter twelve/deployment (#31)
    * Configurations and linting fixes
    
    * Adjust project.json files for build
    
    * Install pnpm and angular-cli-ghpages
    
    * Update packages
    
    * Add APP_BASE_HREF for translations

--- apps/finance/expenses-registration/src/app/transloco-loader.ts
+++ apps/finance/expenses-registration/src/app/transloco-loader.ts
@@ -1,12 +1,14 @@
 import { inject, Injectable } from "@angular/core";
 import { Translation, TranslocoLoader } from "@ngneat/transloco";
 import { HttpClient } from "@angular/common/http";
+import { APP_BASE_HREF } from "@angular/common";
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
+  protected readonly baseHref = inject(APP_BASE_HREF);
 
   getTranslation(lang: 'en' | 'nl') {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(`${this.baseHref}assets/i18n/${lang}.json`);
   }
 }
